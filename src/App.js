8d9cdf8238bdfce7648230b108c33a751ef95e91
Yuliia Yevhenieva <yliaaa3@gmail.com>
2020-11-26 21:14:37 +0200

    made code prettier and working on style

--- src/App.js
+++ src/App.js
@@ -14,25 +14,21 @@ export default class App extends React.Component {
                 changed: false,
             },
             weatherData: {
-                sky: '',
-                temp: '',
-                feelsLike: '',
-                press: '',
-                hum: '',
-                wind: ''
-            },
-            forecastData: {
                 time: [],
                 sky: [],
                 temp: [],
                 feelsLike: [],
                 press: [],
                 hum: [],
-                wind: []
+                wind: [],
+                isReceivedForDay: false,
+                isReceivedForWeek: false
             }
         }
+        this.inputRef = React.createRef();
         this.appId = 'd90eae9f9ee313a6fbfc12712b4104ea';
         this.inputChange = this.inputChange.bind(this);
+        this.clear = this.clear.bind(this);
         this.handleClick = this.handleClick.bind(this);
         this.checkForecast = this.checkForecast.bind(this);
         this.handleKeyPress = this.handleKeyPress.bind(this);
@@ -45,125 +41,122 @@ export default class App extends React.Component {
         this.setState(next);
     }
 
+    clear(){
+        let next = this.state.weatherData;
+        next.time.length = 0;
+        next.sky.length = 0;
+        next.temp.length = 0;
+        next.feelsLike.length = 0;
+        next.press.length = 0;
+        next.hum.length = 0;
+        next.wind.length = 0;
+    }
+
     handleClick(){
         console.log(`Weather in ${this.state.inputValue.value}`)
-        axios.get(`http://api.openweathermap.org/data/2.5/weather?q=${this.state.inputValue.value}&appid=${this.appId}`)
+        if (this.state.weatherData.isReceivedForDay === false) {
+            axios.get(`http://api.openweathermap.org/data/2.5/weather?q=${this.state.inputValue.value}&appid=${this.appId}`)
             .then(res => {
                 console.log(res)
-                let next = this.state;
-                next.weatherData.sky = res.data.weather[0].description;
-                next.weatherData.temp = Math.round(res.data.main.temp - 273.15) + ` C${String.fromCharCode(176)}`;
-                next.weatherData.feelsLike = Math.round(res.data.main.feels_like - 273.15) + ` C${String.fromCharCode(176)}`;
-                next.weatherData.press = res.data.main.pressure + `hPa`;
-                next.weatherData.hum = res.data.main.humidity + `%`;
-                next.weatherData.wind = res.data.wind.speed + `m/s`;
+                this.clear();
+                let next = this.state.weatherData;
+                next.isReceivedForDay = true;
+                next.isReceivedForWeek = false;
+                next.time.push(moment().format('llll'));
+                next.sky.push(res.data.weather[0].description);
+                next.temp.push(Math.round(res.data.main.temp - 273.15));
+                next.feelsLike.push(Math.round(res.data.main.feels_like - 273.15));
+                next.press.push(res.data.main.pressure);
+                next.hum.push(res.data.main.humidity);
+                next.wind.push(res.data.wind.speed);
                 this.setState(next);
             }).catch(err => {
                 alert(`Something went wrong. Could not get weather data in ${this.state.inputValue.value}`)
                 console.error(err);
             })
+        } 
     }
 
     checkForecast(){
         console.log(`Forecast for five days in ${this.state.inputValue.value}`)
+        if (this.state.weatherData.isReceivedForWeek === false) {
         axios.get(`http://api.openweathermap.org/data/2.5/forecast?q=${this.state.inputValue.value}&appid=${this.appId}`)
             .then(res => {
-                console.log(res)
-                let next = this.state;
+                console.log(res);
+                this.clear();
+                let next = this.state.weatherData;
+                next.isReceivedForDay = false;
+                next.isReceivedForWeek = true;
                 for (let i = 0; i <= 39; i++){
-                    next.forecastData.time.push(moment(res.data.list[i].dt).format('llll'));
-                    next.forecastData.sky.push(res.data.list[i].weather[0].description);
-                    next.forecastData.temp.push(Math.round(res.data.list[i].main.temp - 273.15));
-                    next.forecastData.feelsLike.push(Math.round(res.data.list[i].main.feels_like - 273.15));
-                    next.forecastData.press.push(res.data.list[i].main.pressure);
-                    next.forecastData.hum.push(res.data.list[i].main.humidity);
-                    next.forecastData.wind.push(res.data.list[i].wind.speed);
+                    next.time.push(res.data.list[i].dt);
+                    next.sky.push(res.data.list[i].weather[0].description);
+                    next.temp.push(Math.round(res.data.list[i].main.temp - 273.15));
+                    next.feelsLike.push(Math.round(res.data.list[i].main.feels_like - 273.15));
+                    next.press.push(res.data.list[i].main.pressure);
+                    next.hum.push(res.data.list[i].main.humidity);
+                    next.wind.push(res.data.list[i].wind.speed);
                 }
                 this.setState(next);
             }).catch(err => {
                 alert(`Something went wrong. Could not get forecast data in ${this.state.inputValue.value}`)
                 console.error(err);
             })
+        }
     }
 
     handleKeyPress = (e) => {
-        if(e.key === 'Enter'){
-          console.log('enter press here!')
-          this.handleClick();
+        if (e.key === 'Enter'){
+            console.log('The user pressed enter!')
+            if (this.inputRef.current.value.length === 0){
+                alert('Please enter city');
+            } else 
+                this.handleClick();
         }
     }
 
     render () {
+        const state = this.state.weatherData;
         return (
             <div>
             <header>
                 <label htmlFor='cityName'>Enter city: </label>
-                <input id ='cityName' type='text' pattern='A-Za-z' onKeyPress={this.handleKeyPress} required autoFocus value={this.state.inputValue.value} onChange={this.inputChange} ref={ref => this.input = ref} />
+                <input id ='cityName' type='text' pattern='A-Za-z' onKeyPress={this.handleKeyPress} required autoFocus value={this.state.inputValue.value} onChange={this.inputChange} ref={this.inputRef} />
                 <button type='button' onClick={this.handleClick}>Search</button>
             </header>
             <main>
                 <button type='button' onClick={this.handleClick}>Weather today</button>
                 <button type='button' onClick={this.checkForecast}>Forecast for five days</button>
-                <table id='weather'>
-                    <tbody>
+                <table>
+                    <tbody id='weather'>
+                        <tr>
+                            <th>Date and time</th>
+                            {state.time.map(time=><td key={time.id}>{time}</td>)}
+                        </tr>
                         <tr>
                             <th>Sky</th>
-                            <td>{this.state.weatherData.sky}</td>
+                            {state.sky.map(sky=><td key={sky.id}>{sky}</td>)}
                         </tr>
                         <tr>
                             <th>Temperature</th>
-                            <td>{this.state.weatherData.temp}</td>
+                            {state.temp.map(temp=><td key={temp.id}>{temp} С&#176;</td>)}
                         </tr>
                         <tr>
                             <th>Feels like</th>
-                            <td>{this.state.weatherData.feelsLike}</td>
+                            {state.feelsLike.map(feelsLike=><td key={feelsLike.id}>{feelsLike} С&#176;</td>)}
                         </tr>
                         <tr>
                             <th>Atmospheric pressure</th>
-                            <td>{this.state.weatherData.press}</td>
+                            {state.press.map(press=><td key={press.id}>{press} hPa</td>)}
                         </tr>
                         <tr>
                             <th>Humidity</th>
-                            <td>{this.state.weatherData.hum}</td>
+                            {state.hum.map(hum=><td key={hum.id}>{hum} %</td>)}
                         </tr>
                         <tr>
                             <th>Wind</th>
-                            <td>{this.state.weatherData.wind}</td>
+                            {state.wind.map(wind=><td key={wind.id}>{wind} m/s</td>)}
                         </tr>
                     </tbody>
-                </table>
-
-                <table id='forecast'>
-                    <tbody>
-                        <tr>
-                            <th id='date'>Date and time</th>
-                            {this.state.forecastData.time.map(time=><td>{time}</td>)}
-                        </tr>
-                        <tr>
-                            <th>Sky</th>
-                            {this.state.forecastData.sky.map(sky=><td>{sky}</td>)}
-                        </tr>
-                        <tr>
-                            <th>Temperature</th>
-                            {this.state.forecastData.temp.map(temp=><td>{temp} С&#176;</td>)}
-                        </tr>
-                        <tr>
-                            <th>Feels like</th>
-                            {this.state.forecastData.feelsLike.map(feelsLike=><td>{feelsLike} С&#176;</td>)}
-                        </tr>
-                        <tr>
-                            <th>Atmospheric pressure</th>
-                            {this.state.forecastData.press.map(press=><td>{press} hPa</td>)}
-                        </tr>
-                        <tr>
-                            <th>Humidity</th>
-                            {this.state.forecastData.hum.map(hum=><td>{hum} %</td>)}
-                        </tr>
-                        <tr>
-                            <th>Wind</th>
-                            {this.state.forecastData.wind.map(wind=><td>{wind} m/s</td>)}
-                        </tr>
-                        </tbody>
                 </table>
 
             </main>
